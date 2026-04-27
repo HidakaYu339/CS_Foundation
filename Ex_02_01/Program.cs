847c6c7224d35d4255ed36b0cc1a8a779e448011
HidakaYu339 <yumaitake@icloud.com>
2026-04-27 15:47:10 +0900

    first commit

--- Ex_02_01/Program.cs
+++ Ex_02_01/Program.cs
@@ -4,6 +4,12 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int a = 5;
+        int b = 2;
+        float c = (float)a / b;
+        System.Console.Write("c=");
+        System.Console.WriteLine(c);
+
     }
 }
+
