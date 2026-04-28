f5201cb1325ba996d46fff08a1f08089047768c7
HidakaYu339 <yumaitake@icloud.com>
2026-04-28 10:04:35 +0900

    演習３

--- Ex_02_03/Program.cs
+++ Ex_02_03/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Ex_02_03;
+﻿using System.IO.Pipelines;
+
+namespace Ex_02_03;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        System.Console.Write("数値を入力してください->");
+        var number = System.Console.ReadLine();
+        int result;
+        result = int.Parse(number) + 5;
+        System.Console.WriteLine($"5を加算した結果は{result}です");
+        result = int.Parse(number) - 5;
+        System.Console.WriteLine($"5を減算した結果は{result}です");
+
     }
 }
