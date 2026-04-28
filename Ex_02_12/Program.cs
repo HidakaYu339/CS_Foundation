733822cac291dd353880f00cd077a7f3c5ba0578
HidakaYu339 <yumaitake@icloud.com>
2026-04-28 14:39:22 +0900

    演習１２終了

--- Ex_02_12/Program.cs
+++ Ex_02_12/Program.cs
@@ -4,6 +4,13 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("値を入力してください⇒");
+        int inputnum = int.Parse(Console.ReadLine());
+        Console.WriteLine(Mul3(inputnum));
+
+        int Mul3(int num)
+        {
+            return num * 4;
+        }
     }
 }
