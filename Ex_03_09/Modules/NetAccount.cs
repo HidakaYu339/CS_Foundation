69d6e3dc795ecd8519dda70398798ca42b5e77a1
HidakaYu339 <yumaitake@icloud.com>
2026-04-30 17:26:26 +0900

    ４・３０終わり

--- Ex_03_09/Modules/NetAccount.cs
+++ Ex_03_09/Modules/NetAccount.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 
 namespace Ex_03_09
 {
-    public class NetAccount
+    public class NetAccount : Account
     {
-        public int Point { get; set; }
-        ポイントを表すプロパティ
+        public int Point { get; set; }	///ポイントを表すプロパティ
+
+        public NetAccount(string AccountNo, string AccountName, int Balance, int Point) : base(AccountNo, AccountName, Balance)
+        {
+
+        }
     }
 }
