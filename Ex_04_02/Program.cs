cac533ba91951d2ce9258e0977a7055235ea73be
HidakaYu339 <yumaitake@icloud.com>
2026-05-07 11:17:17 +0900

    Ex04_02

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("年を入力してください->");
+        int year = int.Parse(Console.ReadLine());
+        if (DateTime.IsLeapYear(year))
+        {
+            Console.WriteLine($"{year}年は閏年です。");//true処理
+        }
+        else
+        {
+            Console.WriteLine($"{year}年は閏年ではありません。");//false処理
+        }
+    }
+}
